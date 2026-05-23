85676f9c8625d150bd95ab300586caef4664e49e
SanctaGee <tonygabito@gmail.com>
2026-05-23 12:43:57 +0100

    Fixed PersonQueue Enqueue bug - it was inserting at front instead of back

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -13,7 +13,7 @@ public class PersonQueue
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Add(person);  // FIXED: Add to back instead of Insert at front
     }
 
     public Person Dequeue()
